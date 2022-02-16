2c4422a6396cac6cbc6cfbcc734e94f91ee08439
GeraldineBB <geraldine-bornet@hotmail.fr>
2022-02-16 18:11:12 +0100

    style add story section

--- frontend/src/components/AddSection/AddSection.js
+++ frontend/src/components/AddSection/AddSection.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import './AddSection.css';
+
 
 const AddSection = () => {
 
-return (
-    <div>
-        <Link to="/add" className="button is-primary mt-2 mb-2">Ajouter un produit</Link>
-    </div>
-)
+    return (
+        <div className="addSection">
+            <img className="addSection__img" src={`${process.env.PUBLIC_URL}/assets/idea.png`} />
+
+            <Link to="/add" className="button is-primary mt-2 mb-2">Ajouter une histoire</Link>
+        </div>
+    )
 
 }
 
