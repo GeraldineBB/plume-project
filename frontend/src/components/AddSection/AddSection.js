66dff2753cc2dcca90f3dbf1e9f7c4e98e02394d
GeraldineBB <geraldine-bornet@hotmail.fr>
2022-02-18 19:15:21 +0100

    style addSection

--- frontend/src/components/AddSection/AddSection.js
+++ frontend/src/components/AddSection/AddSection.js
@@ -9,8 +9,10 @@ const AddSection = () => {
     return (
         <div className="addSection">
             <img className="addSection__img" src={`${process.env.PUBLIC_URL}/assets/idea.png`} />
-
-            <Link to="/add" className="button is-primary mt-2 mb-2">Ajouter une histoire</Link>
+            <div className="addSection__txt">
+                <p> Vous avez une histoire à raconter ? <br/> Ecrivez là et partager là à la communauté ! </p>
+                <Link to="/add" className="button is-primary mt-2 mb-2">Ajouter une histoire</Link>
+            </div>
         </div>
     )
 
